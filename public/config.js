71fb45edc4f44b6dc4b93fd7ce165c8cd8ace8f3
mealwise <l.tabor@elatec.com>
2025-03-25 22:54:59 +0800

    add secured api endpoint

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.ENVIRONMENT                 = 'LOCAL'
-window.API_BASE_PATH               = 'https://alchives/api'
-window.WEBSOCKET_PATH              = 'https://alchives'
+window.API_BASE_PATH               = 'https://alchives.site/api'
+window.WEBSOCKET_PATH              = 'https://alchives.site'
 window.STORAGE_ACCOUNT             = ''
